3c83bd22738939de5465317447d9fea8a97fba90
treto3469 <101291205+ramon3469@users.noreply.github.com>
2025-08-05 16:48:44 -0300

    lambda_destroy

---
+++ lambda/destroy_env.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+import boto3
+
+BUCKET = "tu-bucket-s3"
+PREFIX = "terraform/envs/qa"
+
+def lambda_handler(event, context):
+    os.chdir("/tmp")
+
+    # Descargar archivos de S3
+    s3 = boto3.client("s3")
+    for key in ["main.tf", "variables.tf", "backend.tf"]:
+        s3.download_file(BUCKET, f"{PREFIX}/{key}", key)
+
+    # Ejecutar Terraform
+    subprocess.run(["terraform", "init"], check=True)
+    subprocess.run(["terraform", "destroy", "-auto-approve"], check=True)
+
+    return {"status": "Entorno QA apagado con éxito"}
